e81de820637a176aa7454387f27af85dca211acf
yawe9101 <ywang@esri.com>
2020-06-09 09:54:02 +0800

    fix ipa plist

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
   debug: process.env.NODE_ENV !== 'production',
   host: process.env.HOST || '0.0.0.0',
-  port: process.env.PORT || 9008,
+  port: parseInt(process.env.PORT) || 9009,
   publicURL: process.env.PUBLIC_URL,
   uploadDir: process.env.UPLOAD_DIR || path.resolve(__dirname, 'upload'),
 }
